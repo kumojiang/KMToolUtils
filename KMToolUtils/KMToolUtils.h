b3abc91d81686e4168053cfa768f75c2d5af0bba
kumo <kumojiang@foxmail.com>
2016-08-22 14:55:44 +0800

    add head file add 3party folder collection some useful source

---
+++ KMToolUtils/KMToolUtils.h
@@ -0,0 +1,19 @@
+//
+//  KMToolUtils.h
+//  KMToolUtilsDemo
+//
+//  Created by kumojiang on 16/8/22.
+//  Copyright © 2016年 kumo. All rights reserved.
+//
+
+#ifndef KMToolUtils_h
+#define KMToolUtils_h
+
+#import "ColorUtils.h"
+#import "DimensionUtils.h"
+
+#import "UIView+KMToolUtils.h"
+#import "UILabel+KMToolUtils.h"
+
+
+#endif /* KMToolUtils_h */
